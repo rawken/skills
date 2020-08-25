8f825d4cb38297755361a5fe5e892bd3472093b2
Roman Kuzmin <romanrus023@gmail.com>
2020-08-25 23:09:12 +0400

    Add about section

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Дата публикации')
+    #cover_img = 
